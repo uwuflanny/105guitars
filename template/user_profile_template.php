d7d9958b1137d741618e1255b63264584d066571
alebrasi <alessandro.brasini1@gmail.com>
2022-01-21 16:03:17 +0100

    Added profile page

---
+++ template/user_profile_template.php
@@ -0,0 +1,60 @@
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-12 col-md-6">
+        
+        <div class="row">
+            <h1 class="text-light">Benvenuto Intruder</h1>
+            <h4 class="text-light">Email: intruder@insideyourwalls.com</h2>
+            <h4 class="text-light">Username: intruder1234</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-md-3 m-4">
+                <div class="card hover-card">
+                    <div class="row"> 
+                        <div class="col-4">
+                            <img src="images/icons/orders-logo.png" class="img-fluid m-3" />
+                        </div>
+                        <div class="col-8 d-flex justify-content-start">
+                            <h5 class="align-self-center">Ordini</h5>
+                        </div>
+                        <a href="#" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 m-4">
+                <div class="card hover-card">
+                    <div class="row"> 
+                        <div class="col-4">
+                            <img src="images/icons/message-icon.png" class="img-fluid m-3" />
+                        </div>
+                        <div class="col-8 d-flex justify-content-start">
+                            <h5 class="align-self-center">Notifiche</h5>
+                        </div>
+                        <a href="#" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-12 col-md-3 m-4">
+                <div class="card hover-card">
+                    <div class="row"> 
+                        <div class="col-4">
+                            <img src="images/icons/cart-icon.png" class="img-fluid m-3" />
+                        </div>
+                        <div class="col-8 d-flex justify-content-start">
+                            <h5 class="align-self-center">Carrello</h5>
+                        </div>
+                        <a href="#" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </div>
+    <div class="col-md-3"></div>
+</div>
