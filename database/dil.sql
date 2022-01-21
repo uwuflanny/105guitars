4e55ea235dc2894e89d8d28b65e01bcae33b0901
uwuflanny <gianlumiglia@gmail.com>
2022-01-21 16:46:59 +0100

    database progress

--- database/dil.sql
+++ database/dil.sql
@@ -5,10 +5,10 @@ INSERT INTO modello (nome, scala, tipo_body, elettronica) VALUES
 ("105 Super Idol", 25.4, "SuperStrat", "Bridge: 105 Duncan Alnico"),
 ("105 Imposter Bass", 24.75, "Clarus", "Bridge: 105 Aktive precision Hmbucker");
 
-INSERT INTO copia (ID_MODELLO, num_corde, colore, materiale) VALUES
-(1, 6, "black matte", 1400),
-(1, 7, "brown sunburst", 1600),
-(2, 6, "crimson red", 1200),
-(2, 8, "black matte", 1600),
-(3, 4, "lime green", 1200),
-(3, 4, "blue sunburst", 1200);
+INSERT INTO copia (ID_MODELLO, num_corde, colore, materiale, prezzo) VALUES
+(1, 6, "black matte", "ebony", 1400),
+(1, 7, "brown sunburst","swamp ash", 1600),
+(2, 6, "crimson red","ebony", 1200),
+(2, 8, "black matte","stainless steel", 10000),
+(3, 4, "lime green","plywood", 100),
+(3, 4, "blue sunburst","ebony", 1200);
