5e624c2cbb9c9fbc7aaf653a389dc7e32f94ebc4
uwuflanny <gianlumiglia@gmail.com>
2022-01-21 16:40:33 +0100

    database progress

---
+++ database/dil.sql
@@ -0,0 +1,14 @@
+use 105guitars;
+
+INSERT INTO modello (nome, scala, tipo_body, elettronica) VALUES 
+("105 V Spektre", 25.5, "Flying V", "Neck & Bridge: 105 Duncan Alnico; 5-way-blade"),
+("105 Super Idol", 25.4, "SuperStrat", "Bridge: 105 Duncan Alnico"),
+("105 Imposter Bass", 24.75, "Clarus", "Bridge: 105 Aktive precision Hmbucker");
+
+INSERT INTO copia (ID_MODELLO, num_corde, colore, materiale) VALUES
+(1, 6, "black matte", 1400),
+(1, 7, "brown sunburst", 1600),
+(2, 6, "crimson red", 1200),
+(2, 8, "black matte", 1600),
+(3, 4, "lime green", 1200),
+(3, 4, "blue sunburst", 1200);
