a6f8eb291bf28b291c1287ff004e73479b87c2d3
uwuflanny <gianlumiglia@gmail.com>
2022-01-21 15:54:10 +0100

    css link fix

--- template/base.php
+++ template/base.php
@@ -3,31 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="template/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>105°C Guitars</title>
-
-        <style>
-            body {
-                color: white;
-                background-image: url("images/grey.jpg");
-            }
-
-            main {
-                margin-top: 10em;
-                overflow: hidden;
-            }
-
-            .img-icon {
-                width: 3em;
-                height: 3em;
-            }
-
-            .img-gallery {
-                width: 20%;
-            }
-
-            
-        </style>
     </head>
 
     <body>
