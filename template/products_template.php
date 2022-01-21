f2e7d31af8770ead7e914547270a027ce18f56f6
uwuflanny <gianlumiglia@gmail.com>
2022-01-21 14:51:57 +0100

    sus

--- template/products_template.php
+++ template/products_template.php
@@ -3,42 +3,42 @@
 		<div class="container">
 			<div class="row g-1">
 				<div class="col-md-3">
-					<div class="card p-3 mt-5 me-3 ms-3">
+					<div class="card p-3 mt-5 me-3 ms-3 card_product">
 						<div class="text-center" class="bg-transparent"> 
 							<img src="images/guitar3.png" class="card-img-top w-50"> 
 						</div>
 						<div class="product-details"> 
-							<span class="font-weight-bold d-block text">$ 7.00 <br>solar megaa swagger</span>
+							<span class="font-weight-bold d-block product_text">$ 7.00 <br>solar megaa swagger</span>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-3">
-					<div class="card p-3 mt-5 me-3 ms-3">
+					<div class="card p-3 mt-5 me-3 ms-3 card_product">
 						<div class="text-center" class="bg-transparent"> 
 							<img src="images/guitar3.png" class="card-img-top w-50"> 
 						</div>
 						<div class="product-details"> 
-							<span class="font-weight-bold d-block text">$ 7.00 <br>solar megaa swagger</span>
+							<span class="font-weight-bold d-block product_text">$ 7.00 <br>solar megaa swagger</span>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-3">
-					<div class="card p-3 mt-5 me-3 ms-3">
+					<div class="card p-3 mt-5 me-3 ms-3 card_product">
 						<div class="text-center" class="bg-transparent"> 
 							<img src="images/guitar3.png" class="card-img-top w-50"> 
 						</div>
 						<div class="product-details"> 
-							<span class="font-weight-bold d-block text">$ 7.00 <br>solar megaa swagger</span>
+							<span class="font-weight-bold d-block product_text">$ 7.00 <br>solar megaa swagger</span>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-3">
-					<div class="card p-3 mt-5 me-3 ms-3">
+					<div class="card p-3 mt-5 me-3 ms-3 card_product">
 						<div class="text-center" class="bg-transparent"> 
 							<img src="images/guitar3.png" class="card-img-top w-50"> 
 						</div>
 						<div class="product-details"> 
-							<span class="font-weight-bold d-block text">$ 7.00 <br>solar megaa swagger</span>
+							<span class="font-weight-bold d-block product_text">$ 7.00 <br>solar megaa swagger</span>
 						</div>
 					</div>
 				</div>
