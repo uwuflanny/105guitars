756d6ecd0d516f6154ed56f20604af9ec3d2a3d0
alebrasi <alessandro.brasini1@gmail.com>
2022-01-27 17:41:17 +0100

    Modified base page

--- template/base.php
+++ template/base.php
@@ -1,9 +1,14 @@
+<?php
+    $isCartEmpty = !isset($params["articles-in-cart"]) && ($params["articles-in-cart"] > 0);
+
+?>
+
 <!doctype html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="template/style.css">
+        <link rel="stylesheet" href="template/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>105°C Guitars</title>
         <script
@@ -22,17 +27,38 @@
             </button>
             <div class="navbar-collapse collapse" id="navbarCollapse"> 
                 <ul class="navbar-nav mr-auto display-5">
-                    <li class="nav-item border-left border-secondary px-3"><a class="nav-link" href="products.php"><h4>Products</h4></a></li>
-                    <li class="nav-item border-left border-secondary px-3"><a class="nav-link" href="#"><h4>Quality Check</h4></a></li>
-                    <li class="nav-item border-left border-secondary px-3"><a class="nav-link" href="#"><h4>About Us</h4></a></li>                  
+                    <li class="nav-item border-left border-secondary px-3">
+                        <a class="nav-link" href="products.php">
+                            <h4>Products</h4>
+                        </a>
+                    </li>
+                    <li class="nav-item border-left border-secondary px-3">
+                        <a class="nav-link" href="#">
+                            <h4>Quality Check</h4>
+                        </a>
+                    </li>
+                    <li class="nav-item border-left border-secondary px-3">
+                        <a class="nav-link" href="#">
+                            <h4>About Us</h4>
+                        </a>
+                    </li>                  
                 </ul>
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item border-left border-secondary px-3"><a class="nav-link" href="#"><h4>Intruder Profile</h4></a></li>
+                    <li class="nav-item border-left border-secondary px-3">
+                        <a class="nav-link" href="<?php echo $params["profile-url"];?>">
+                            <h4><?php echo $params["logged-user"]." "; ?>Profile</h4>
+                        </a>
+                    </li>
                     <li class="nav-item border-left border-secondary navbar-brand px-3"> 
-                        <a href="#" class="position-relative">
+                        <a href="cart.php" class="position-relative">
                             <img src="images/icons/cart-icon-navbar.png" style="height: 30px;" alt="shopping-cart">
-                                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">1<span class="visually-hidden">Articles in cart</span></span>
-                            </img>
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">
+                                <?php if(!$isCartEmpty) {
+                                       echo $params["articles-in-cart"]; 
+                                    }
+                                ?>
+                                <span class="visually-hidden">Articles in cart</span>
+                            </span>
                         </a>
                     </li>
                 </ul>
@@ -41,7 +67,7 @@
         </nav>
 
         <main>
-        <?php if (isset($params["name"])) { require($params["name"]); } ?>
+            <?php if (isset($params["name"])) { require($params["name"]); } ?>
         </main>
         <script src="js/asd.js" type="text/javascript"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
---
+++ template/base_page.php
@@ -0,0 +1,8 @@
+<?php 
+
+$params["articles-in-cart"] = 0;
+$params["profile-url"] = "login.php";
+$params["logged-user"] = "";
+
+require "base.php";
+?>
