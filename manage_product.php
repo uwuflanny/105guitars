125d8690d99f4bdc75be41b3c65f837200cf701e
uwuflanny <gianlumiglia@gmail.com>
2022-01-30 17:58:25 +0100

    comment

--- manage_product.php
+++ manage_product.php
@@ -15,6 +15,9 @@ if(!isset($_SESSION["isadmin"]) || $_SESSION["isadmin"] == false || empty($_SESS
         $uploadedside = uploadImage("./images/products/", $_FILES["side-photo"]);
         $uploadedback = uploadImage("./images/products/", $_FILES["back-photo"]);
 
+        // se una immagine viene copiata ma le altre no, non viene fatta la insert e il form dovra' riessere compilato.
+        // l'immagine copiata alla prima richiesta non viene rimossa, ma alla seconda richiesta, verrà assegnato un nuovo nome alla nuova immagine,
+        // nella cartella ci sarà così un'immagine duplicata inutile ma non dannosa
         if($uploadedfront[0] && $uploadedside[0] && $uploadedback[0]) {
             $the_db->addCopy($_POST["series"], $_POST["number-strings"], $_POST["color"], $_POST["body-material"], $_POST["price"], $uploadedfront[1], $uploadedside[1], $uploadedback[1]);
         }
