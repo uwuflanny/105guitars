d84c6302c900894ba97fef3858b14ffcf3084287
uwuflanny <gianlumiglia@gmail.com>
2022-01-31 15:14:37 +0100

    added 404 page

---
+++ 404.php
@@ -0,0 +1,4 @@
+<?php
+$params["name"] = "404_template.php";
+require "template/base_page.php"
+?>
--- view_product.php
+++ view_product.php
@@ -2,10 +2,18 @@
 //require_once "database.php";
 require_once "bootstrap_page.php";
 
+function alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+
 $params["name"] = "view_product_template.php";
 $params["scripts"] = ["js/view_product.js"];
 $productSpecifications = $the_db->getProductSpecifications($_GET["serial"])[0];
-// $productInfo = $the_db->getProducts($_GET["serial"])[0];
+
+if(empty($productSpecifications)){
+    header('Location: 404.php');
+    return;
+}
 
 $params["price"] = $productSpecifications["Price"];
 $params["color"] = $productSpecifications["Color"];
