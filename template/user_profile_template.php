e3423199d1c914d3e8b1f0415408e6a5e1d233ed
uwuflanny <gianlumiglia@gmail.com>
2022-02-01 16:17:19 +0100

    added toast notifications template

--- template/user_profile_template.php
+++ template/user_profile_template.php
@@ -2,73 +2,91 @@
     <div class="col-md-3"></div>
     <div class="col-12 col-md-6">
         
-        <div class="row">
-            <h1 class="text-light">Benvenuto Intruder</h1>
-            <h4 class="text-light">Email: intruder@insideyourwalls.com</h4>
-            <h4 class="text-light">Username: intruder1234</h4>
+        <div class="row mt-5">
+            <h1 class="text-light d-flex justify-content-center">Benvenuto <?php echo $_SESSION["name"];?></h1>
+            <h4 class="text-light d-flex justify-content-center">Email: <?php echo $_SESSION["email"];?></h4>
         </div>
 
-        <div class="row">
-            <div class="col-12 col-md-3 m-4">
-                <div class="card hover-card">
-                    <div class="row"> 
-                        <div class="col-4">
-                            <img src="images/icons/orders-logo.png" class="img-fluid m-3" />
+        <div class="container  mb-5">
+            <div class="row">
+                <div class="col">
+
+
+                        <div class="card hover-card user_profile_card">
+                            <div class="row"> 
+                                <div class="col-3">
+                                    <img src="images/icons/message-icon.png" class="img-fluid m-3" />
+                                </div>
+                                <div class="col-8 d-flex justify-content-start">
+                                    <h5 class="align-self-center">Notifiche</h5>
+                                </div>
+                                <a class="stretched-link" data-bs-toggle="collapse" href="#collapseNotifications" role="button" aria-expanded="false" aria-controls="collapseNotifications"></a>
+                            </div>
+                            <div class="row collapse justify-content-center mb-3" id="collapseNotifications">
+                                <div class="toast show">
+                                    <div class="toast-header">
+                                        Notifica 2
+                                        <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
+                                    </div>
+                                    <div class="toast-body">
+                                        Ordine arrivato!s
+                                    </div>
+                                </div>
+                                <div class="toast show">
+                                    <div class="toast-header">
+                                        Notifica 1
+                                        <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
+                                    </div>
+                                    <div class="toast-body">
+                                        Ordine spedito!
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-8 d-flex justify-content-start">
-                            <h5 class="align-self-center">Ordini</h5>
+
+
+                        <div class="card hover-card mt-3 user_profile_card">
+                            <div class="row"> 
+                                <div class="col-3">
+                                    <img src="images/icons/orders-logo.png" class="img-fluid m-3" />
+                                </div>
+                                <div class="col-8 d-flex justify-content-start">
+                                    <h5 class="align-self-center">Ordini</h5>
+                                </div>
+                                <a href="#" class="stretched-link"></a>
+                            </div>
+                        </div>                                   
+
+
+                        <div class="card hover-card mt-3 ">
+                            <div class="row"> 
+                                <div class="col-3">
+                                    <img src="images/icons/cart-icon.png" class="img-fluid m-3" />
+                                </div>
+                                <div class="col-8 d-flex justify-content-start">
+                                    <h5 class="align-self-center">Carrello</h5>
+                                </div>
+                                <a href="#" class="stretched-link"></a>
+                            </div>
                         </div>
-                        <a href="#" class="stretched-link"></a>
+
+
+                        <div class="card hover-card mt-3 user_profile_card">
+                            <div class="row"> 
+                                <div class="col-3">
+                                    <img src="images/icons/logout.png" class="img-fluid m-3" />
+                                </div>
+                                <div class="col-8 d-flex justify-content-start">
+                                    <h5 class="align-self-center">LOGOUT</h5>
+                                </div>
+                                <a href="login.php" class="stretched-link"></a>
+                            </div>
+                        </div>
+
+                        
                     </div>
                 </div>
             </div>
-
-            <div class="col-12 col-md-3 m-4">
-                <div class="card hover-card">
-                    <div class="row"> 
-                        <div class="col-4">
-                            <img src="images/icons/message-icon.png" class="img-fluid m-3" />
-                        </div>
-                        <div class="col-8 d-flex justify-content-start">
-                            <h5 class="align-self-center">Notifiche</h5>
-                        </div>
-                        <a href="#" class="stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
-             <div class="col-12 col-md-3 m-4">
-                <div class="card hover-card">
-                    <div class="row"> 
-                        <div class="col-4">
-                            <img src="images/icons/cart-icon.png" class="img-fluid m-3" />
-                        </div>
-                        <div class="col-8 d-flex justify-content-start">
-                            <h5 class="align-self-center">Carrello</h5>
-                        </div>
-                        <a href="#" class="stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-md-3 m-4">
-                <div class="card hover-card">
-                    <div class="row"> 
-                        <div class="col-4">
-                            <img src="images/icons/logout.png" class="img-fluid m-3" />
-                        </div>
-                        <div class="col-8 d-flex justify-content-start">
-                            <h5 class="align-self-center">LOGOUT</h5>
-                        </div>
-                        <a href="login.php" class="stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
         </div>
-
-
-
-    </div>
     <div class="col-md-3"></div>
 </div>
