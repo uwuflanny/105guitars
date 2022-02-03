57563503cbf9bc176654015c5e19941feb45675e
alebrasi <alessandro.brasini1@gmail.com>
2022-02-03 15:52:39 +0100

    Re-reworked cart

--- cart.php
+++ cart.php
@@ -3,17 +3,21 @@ require_once "bootstrap_page.php";
 
 
 //TODO: fare la generazione della tabella dinamicamente con ajax
+//inserire token
+$params["name"] = "cart_template.php";
 if(is_set_and_not_empty($_SESSION["articles-in-cart"])) {
-    $params["name"] = "cart_template.php";
     $params["articles"] = [];
-    
+    $token = bin2hex(random_bytes(16));
+    $_SESSION["token"] = $token;
+    $params["total-cost"] = 0;
     foreach($_SESSION["articles-in-cart"] as $articleSerial) {
         $temp = new stdClass();
         $product = $the_db->getProductSpecifications($articleSerial)[0];
         $temp->img = $product["side_image"];
-        $temp->namee = $product["Name"];
+        $temp->namee = $product["Name"]."-".$product["Color"];
         $temp->price = $product["Price"];
         array_push($params["articles"], $temp);
+        $params["total-cost"] += $product["Price"];
     }
 
 }
--- template/cart_template.php
+++ template/cart_template.php
@@ -2,73 +2,47 @@
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-lg-10 col-xl-8">
-            <!--
-                <div class="card bg-dark">
-                    <div class="card-header px-4 py-5">
+                <article class="card bg-dark">
+                    <header class="card-header px-4 py-5">
                         <h5 class="mb-0 text-light">Cart</h5>
-                    </div>
+                    </header>
 
                     <div class="card-body p-4">
                         <div class="card shadow-0 border mb-4">
                             <div class="card-body">
+                                <?php if(is_set_and_not_empty($params["articles"])):?>
                                 <?php foreach($params["articles"] as $article): ?>
-                                    <div class="row">
+                                    <article class="row">
                                         <div class="col-md-3">
-                                            <img src="<?php echo "./images/products/".$article->img; ?>" class="img-fluid" alt="Phone">
+                                            <img src="<?php echo "./images/products/".$article->img; ?>" class="img-fluid" alt="">
                                         </div>
-                                        <div class="col-md-3 text-center d-flex justify-content-center align-items-center">
+                                        <div class="col-md-5 text-center d-flex justify-content-center align-items-center">
                                             <p class="text-muted mb-0"><?php echo $article->namee; ?></p>
                                         </div>
-                                        <div class="col-md-3 text-center d-flex justify-content-center align-items-center">
+                                        <div class="col-md-2 text-center d-flex justify-content-center align-items-center">
                                             <p class="text-muted mb-0 small"><?php echo $article->price."$"; ?></p>
                                         </div>
-                                        <div class="col-md-3 d-flex justify-content-center align-items-center">
-                                            <button type="button" class="btn btn-danger">Rimuovi</button>
+                                        <div class="col-md-2 d-flex justify-content-center align-items-center">
+                                            <button type="button" class="btn btn-danger">Remove</button>
                                         </div>
-                                    </div>
+                                    </article>
                                 <?php endforeach; ?>
+                                <?php else:?>
+                                    <p>Your cart is empty :(</p>
+                                <?php endif;?>
                             </div>
  
-                            <div class="d-flex justify-content-between p-2">
-                                <button type="button" class="btn btn-success w-25 p-3 ms-4">Compra</button>
-                                <p class="text-light mb-0 w-25 p-3"><span class="fw-bold me-4 ">Total</span> $898.00</p>
-                            </div>
+                           
                         </div>
+                           
                     </div>
-                
-                </div>
-                -->
-
-                <article>
-                    <header>
-                        <h2 class="text-light">Cart</h2>
-                    </header>
-
-                    <section class="cart-section">
-                        <table class="table table-dark table-stripe table-striped">
-                            <thead>
-                                <th scope="col">Product image</th>
-                                <th scope="col">Product</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Action</th>
-                            </thead>
-                            <tbody>
-                            <?php foreach($params["articles"] as $article): ?>
-                                <tr>
-                                    <td><img src="<?php echo "./images/products/".$article->img; ?>" class="" style="width: 60%;"></td>
-                                    <td class=""><?php echo $article->namee; ?></td>
-                                    <td><?php echo $article->price."$"; ?></td>
-                                    <td><button type="button" class="btn btn-danger">Rimuovi</button></td>
-                                </tr>
-                            <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </section>
-                    <footer>
-                        <button type="button" class="btn btn-success w-25">Compra</button>
-                    </footer>
+                        <?php if(is_set_and_not_empty($params["articles"])):?>
+                        <footer class="d-flex justify-content-between p-2">
+                            <button type="button" class="btn btn-success w-25 p-3 ms-4">Buy now</button>
+                            <p class="text-light mb-0 w-25 p-3"><span class="fw-bold me-4 ">Total</span><?php echo $params["total-cost"]."$";?></p>
+                        </footer>
+                        <?php endif;?>
                 </article>
-
 
             </div>
         </div>
