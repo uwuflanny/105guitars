e8709deb8f4fee16c3b2d15bfdba6dc692191c96
chrg <christian.ricci2000@gmail.com>
2022-02-03 22:58:59 +0100

    dummy fix

--- bootstrap_page.php
+++ bootstrap_page.php
@@ -1,7 +1,6 @@
 <?php
 require_once "database.php";
 require_once "functions.php";
-$the_db = new Database("localhost", "root", "toor", "105guitars", 3306);
+$the_db = new Database("localhost", "root", "", "105guitars", 3306);
 session_start();
-
 ?>
